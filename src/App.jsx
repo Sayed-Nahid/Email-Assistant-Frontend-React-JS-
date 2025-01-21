1daf68d8c45624ecd2ab6c7b1a72e10aa97f4fe3
Sayed Nahid <nahid15-3849@diu.edu.bd>
2025-01-21 18:14:56 +0600

    User can see Generated Reply by Gemini based to the tone user seted and also a button is added for copying genereted reply to the clipboard

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,31 @@ function App() {
               {loading? <CircularProgress size={24}/> : "Generate Reply"}
             </Button>
         </Box>
+        {error && (
+          <Typography color='error' sx={{ mb:2 }}>
+            {error}
+          </Typography>
+        )}
+        {generatedReply && (
+          <Box sx={{ mt: 3}}>
+            <Typography variant='h6' gutterBottom>
+              Generated Reply:
+            </Typography>
+            <TextField 
+              fullWidth
+              multiline
+              rows={6}
+              variant='outlined'
+              value={generatedReply || ''}
+              inputProps={{ readOnly: true }}/>
+              <Button
+                variant='outlined'
+                sx={{ mt:2 }}
+                onClick={() => navigator.clipboard.writeText(generatedReply)}>
+                Copied to Clipboard
+              </Button>
+          </Box>
+        )}
       </Container>
   )
 }
